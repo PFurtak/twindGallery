2351c77e17ac0b79bc0ea88c92c27499e725e5d5
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-10 00:08:52 -0400

    brought in loading and search result conditional, created search compnent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Card from './components/Card';
+import Search from './components/Search';
 
 function App() {
   const [images, setImages] = useState([]);
@@ -16,16 +17,28 @@ function App() {
         setIsLoading(false);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [term]);
 
   return (
     <div className='App'>
       <div className='container mx-auto'>
-        <div className='grid grid-cols-3 gap-4'>
-          {images.map((image) => (
-            <Card key={image.id} image={image} />
-          ))}
-        </div>
+        <Search searchText={(text) => setTerm(text)} />
+
+        {!isLoading && images.length === 0 && (
+          <h1 className='text-5xl text-center mx-auto mt-32'>
+            Image results not found
+          </h1>
+        )}
+
+        {isLoading ? (
+          <h1 className='text-6xl text-center mx-auto mt-32'>Loading</h1>
+        ) : (
+          <div className='grid grid-cols-3 gap-4'>
+            {images.map((image) => (
+              <Card key={image.id} image={image} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
