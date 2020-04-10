c8751083204f4023e3bc43f82888ad3c4ecd5f28
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-09 20:51:35 -0400

    started working on card

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import React from 'react';
 function App() {
   return (
     <div className='App'>
-      <h1 className='text-3xl'>My App!</h1>
+      <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+        <img
+          src='https://source.unslpash.com/random'
+          alt=''
+          className='w-full'></img>
+        <div className='px-6 py-4'>
+          <div className='font-bold text-purple-500 text-xl mb-2'>
+            Photo by John
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
